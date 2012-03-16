97c5f97af673f5ab5dbfd841d50d8448837afbbc
kichon <kikuchi.yoichi@gmail.com>
2012-03-16 16:17:26 +0900

    余計なフォルダの削除

--- DB/backup/pdo2.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$dbh = new PDO('mysql:host=localhost;dbname=twit', 'root', '');
--- DB/backup/sample1.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'Db.class.php';
-
-$manager = Db::getInstance();
-$manager->connect();
-
-$res = $manager->findAll();
-
-var_dump($res);
